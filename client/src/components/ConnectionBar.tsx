a71b02bd0c8ad7481d0eb35612070c9e98ccf401
siekiera-a <siekiera.a31@gmail.com>
2021-05-13 16:17:43 +0200

    ConnectionBar

--- client/src/components/ConnectionBar.tsx
+++ client/src/components/ConnectionBar.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { connectionContext } from '../ConnectionContext';
 
 export default function ConnectionBar() {
+  const { serverAddress } = useContext(connectionContext);
+
   return (
-    <View>
-      <Text>Connection Bar</Text>
+    <View style={styles.container}>
+      <Text>{serverAddress}</Text>
     </View>
   );
 }
+
+const padding = 20;
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    backgroundColor: '#CCC',
+    alignItems: 'center',
+    paddingTop: padding,
+    paddingBottom: padding,
+  },
+});
